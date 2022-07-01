751eb0fe6e37d07ce1a72e006c32c58ffafc1992
lmrang <limlr@naver.com>
2022-07-01 09:05:56 +0900

    25번 문제 수정 완료

--- AA/AA/25번.cpp
+++ AA/AA/25번.cpp
@@ -1,42 +1,10 @@
-//25. 석차 구하기\
+//25. 석차 구하기 (브루투포스)
 //50등대가 한명도 보이지 않음 다시 수정해야함
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include<stdio.h>
-#include<vector>
 int grade[101];
 int num[101];
-
-void quick(int* grade, int start, int end)
-{//내림차순 정렬
-	if (start >= end) return;
-
-	int key = start;
-	int i = start + 1;
-	int j = end;
-	int temp;
-
-	while (i <= j)
-	{
-		while (i <= end && grade[i] >= grade[key]) i++;
-		while (j > start && grade[j] <= grade[key]) j--;
-		if (i > j)
-		{
-			temp = grade[key];
-			grade[key] = grade[j];
-			grade[j] = temp;
-		}
-		else
-		{
-			temp = grade[i];
-			grade[i] = grade[j];
-			grade[j] = temp;
-		}
-	}
-
-	quick(grade, start, j - 1);
-	quick(grade, j + 1, end);
-}
 
 int main()
 {
@@ -46,24 +14,50 @@ int main()
 	for (int i = 1; i <= n; i++)
 	{
 		scanf("%d", &num[i]);
-		grade[i] = num[i];
+		grade[i] = 1;
 	}
 
-	quick(grade, 1, n);		//퀵 내림차순 정렬
-
-	//for (int i = 1; i <= n; i++)
-	//{
-	//	for (int j = 1; j <= n; j++)
-	//	{
-	//		if (grade[i-1] == grade[i]) continue;	//동점자 판단
-	//		if (grade[i] == num[j])
-	//		{
-	//			num[j] = i;
-	//		}
-	//	}
-	//}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (num[j] > num[i]) grade[i]++;
+		}
+	}
 
 	for (int i = 1; i <= n; i++) printf("%d ", grade[i]);
 
 	return 0;
 }
+
+//정렬 사용 X
+//void quick(int* grade, int start, int end)
+//{//내림차순 정렬
+//	if (start >= end) return;
+//
+//	int key = start;
+//	int i = start + 1;
+//	int j = end;
+//	int temp;
+//
+//	while (i <= j)
+//	{
+//		while (i <= end && grade[i] >= grade[key]) i++;
+//		while (j > start && grade[j] <= grade[key]) j--;
+//		if (i > j)
+//		{
+//			temp = grade[key];
+//			grade[key] = grade[j];
+//			grade[j] = temp;
+//		}
+//		else
+//		{
+//			temp = grade[i];
+//			grade[i] = grade[j];
+//			grade[j] = temp;
+//		}
+//	}
+//
+//	quick(grade, start, j - 1);
+//	quick(grade, j + 1, end);
+//}
