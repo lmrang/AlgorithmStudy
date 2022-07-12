0c833d014eca48185eff31bf7cf41a47e3a93730
lmrang <limlr@naver.com>
2022-07-12 22:54:14 +0900

    27번 소인수 분해 응용 문제 (완료)

--- AA/AA/27번.cpp
+++ AA/AA/27번.cpp
@@ -1,37 +1,47 @@
+//27번. N!의 표현법
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main()
 {
 	int a;
-	int num[1001] = { 0 };
-	int num2[1001] = { 0 };
-	int ans[1001] = { 0 };
-	int j = 0;
 	scanf("%d", &a);
-
-	for (int i = 1; i <= a; i++)
-	{//약수 구하기
-		for (int j = i; j <= a; j += i)
-		{
-			num[j]++;
-		}
-	}
+	vector<int> ch(a + 1);
 
 	for (int i = 2; i <= a; i++)
 	{
-		if (num[i] == 2)
-		{//소수만
-			ans[j]++;
-			j++;
-		}
-		else
+		int temp = i;
+		int j = 2;
+		while (1)
 		{
-
+			if (temp % j == 0)
+			{//나누어 떨어지면
+				temp = temp / j;
+				ch[j]++;
+			}
+			else j++; //나누어 떨어지지 않으면
+			if (temp == 1) break;
 		}
+	}
+
+	printf("%d! = ", a);
+	for (int i = 2; i <= a; i++)
+	{
+		if (ch[i] != 0) printf("%d ", ch[i]);
 	}
 
 	return 0;
 }
+
+/*
+*소인수 분해 응용*
+나오는 인수를 체크배열에 추가한다
+
+소인수 분해
+2로 나누어서 몫이 딱 맞아 떨어지지 않으면 2->3으로 증가
+딱 맞아 떨어지면 증가 시키지 않고 그대로 계속 나눈다.
+2->3->4(4는 2로 나누어지는 숫자이기에 4로 나누어 떨어질 숫자는 없다)
+*/
